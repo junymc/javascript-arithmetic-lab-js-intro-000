478ffe167a325ac2f7d933d15091470b3101f65f
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-15 21:11:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+  return a + b
+}
+
+function suntract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}  
+
+function inc(number) {
+  return number + 1
+}
+
+function dec(number) {
+  return number - 1
+}
+
+function makeInt(number) {
+  return parseInt(number, 10)
+}
